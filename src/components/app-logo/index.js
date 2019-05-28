d072acd2d83182596dec036f545330a2603b61c5
sultan99 <sultan99@mail.ru>
2019-05-28 17:08:35 +0600

    App logo added.

---
+++ src/components/app-logo/index.js
@@ -0,0 +1,11 @@
+import Circle from './circle.sc'
+import React from 'react'
+import logo from './logo.png'
+
+const AppLogo = () => (
+  <Circle>
+    <img src={logo}/>
+  </Circle>
+)
+
+export default AppLogo
